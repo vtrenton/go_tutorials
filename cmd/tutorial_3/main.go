e861d477ebbc9636ae7e367faabbcce46f91ce25
Trent V <trenton.vanderwert@gmail.com>
2023-08-18 22:13:39 -0500

    added a remainder to intdivision

--- cmd/tutorial_3/main.go
+++ cmd/tutorial_3/main.go
@@ -7,6 +7,13 @@ func main() {
 	var printValue string = "Hello World!"
 	// note that the type is enforced so I can only pass in a string
 	printMe(printValue)
+
+	numerator := 11
+	denominator := 2
+
+	// in a function that returns mutliple values I can inline assign multiple values similtaniously
+	result, remainder := intDivision(numerator, denominator)
+	fmt.Printf("The result of the integer division is %v with the remainder of %v", result, remainder)
 }
 
 // printValue is of type string
@@ -16,7 +23,12 @@ func printMe(printValue string) {
 
 // when returing a value you need specify the type of the return
 // func <funcname>(<param1> <type1>, <param2> <type2>) <return_type>
-func intDivision(numerator int, denomonator int) int {
+// We can specify multiple types if there are multiple returns
+// we can do this by putting the return types into parenthesis
+// func <funcname>(<param1> <type1>, <param2> <type2>) (<return_type1>, <return_type2>))
+func intDivision(numerator int, denomonator int) (int, int) {
 	var result int = numerator / denomonator
-	return result
+	var remainder int = numerator % denomonator
+	// return multiple values with the comma
+	return result, remainder
 }
